c6d366d6bece08285265095bcb7b96b6c406fb45
QamarTheCoder <qamarthecoder69@gmail.com>
2025-10-03 13:12:02 +0500

    Explore Page Completed

---
+++ frontend/src/dashboard/Explore/ExploreComponent.js
@@ -0,0 +1,22 @@
+import React from "react";
+import './ExploreComponent.css'
+
+export default function ExploreComponenet({post}){
+    return(
+        <div className="p-1 explore-container">
+            <div className="explore-overlay">
+                <p><i className="fa-solid fa-heart"></i> {post.likes}</p>
+                <p><i className="fa-solid fa-comment"></i> {post.comments.length}</p>
+            </div>
+            <img  
+                src={post.post} 
+                style={{
+                    width:"100%",
+                    height:"100%",
+                    objectFit:"cover",
+                    objectPosition:"center"
+                }}
+            />
+        </div>
+    )
+}
