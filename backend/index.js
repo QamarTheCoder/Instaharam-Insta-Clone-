bcf59c714744c96c3f4332018b3f304c7770edac
QamarTheCoder <qamarthecoder69@gmail.com>
2025-10-08 19:38:44 +0500

    Authentication

--- backend/index.js
+++ backend/index.js
@@ -1,17 +1,109 @@
 require('dotenv').config()
 
 const express= require("express")
+const cors= require('cors')
 const mongoose= require('mongoose')
+const session=require('express-session')
 const UserModel=require('./Model/UserSchema')
 const PostModel=require('./Model/PostSchema')
 const CommentModel=require('./Model/CommentSchema')
+const passport=require('passport')
+const LocalStrategy=require('passport-local')
+const sessionOptions={
+    secret:'mysupasceretkey',
+    resave:false,
+    saveUninitialized:true,
+    cookie:{
+        expires:Date.now() +7  * 24  * 60 * 60 *1000,
+        maxAge:7  * 24  * 60 * 60 *1000,
+        httpOnly:true
+    }
+}
+
 
 const app=express()
 const PORT= process.env.PORT || 2020;
-const MONGO_URL=process.env.MONGO_URL 
+const MONGO_URL=process.env.MONGO_URL
+
+
+
+app.use(cors({ origin: "http://localhost:3000", credentials: true }))
+app.use(express.json())
+app.use(express.urlencoded({extended:true}))
+
+app.use(session(sessionOptions));
+app.use(passport.initialize())
+app.use(passport.session())
+passport.use(new LocalStrategy(UserModel.authenticate()))
+passport.serializeUser(UserModel.serializeUser())
+passport.deserializeUser(UserModel.deserializeUser())
+
+
+app.post('/user/signupUser',async(req,res)=>{
+    try{
+    const {email,username,password}=req.body;
+    const User= new UserModel({
+        email,
+        username
+    })
+    const registeredUser=await UserModel.register(User,password)
+    req.login(registeredUser,(e)=>{
+        if (e){
+ res.status(400).json({
+            success:false,
+            message:'User Already Exists'
+        })
+        }else{
+
+              res.status(201).json({
+                success:true,
+                message:'User Registered & loggedin Successfully'
+            })
+
+        }
+       
+      
+    })
+    
+    }catch(e){
+        res.status(400).json({
+            success:false,
+            message: e.message
+        })
+    }
+})
+
+app.post('/user/login',passport.authenticate("local",{failureMessage:'Incorrect Credentials'}),async(req,res)=>{
+  
+        console.log(` CURRENT USER FROM COOkIE${req.user}`)
+        res.status(201).json({
+            success:true,
+            message:'User Loggedin Successfully'
+        })
+        
+        
+})
+
+app.post('/user/Logout',(req,res)=>{
+    req.logOut((err)=>{
+        if (err){
+        return next(err)
+        }
+        req.session.destroy(()=>{
+            res.clearCookie('connect.sid')
+            return res.status(201).json({
+                success:true,
+                message:'LoggedOut Successfully'
+            })
+        })
+    })
+})
+
 app.listen(PORT, ()=>{
     console.log('starting App')
     mongoose.connect(MONGO_URL)
     console.log('DB connected')
 
 })
+
+
--- frontend/src/dashboard/Menu/Menu.js
+++ frontend/src/dashboard/Menu/Menu.js
@@ -1,10 +1,30 @@
+import axios from "axios";
 import React from "react";
+import { useNavigate } from "react-router-dom";
+import {toast, ToastContainer} from 'react-toastify'
 
 export default function Menu(){
+    let navigate=useNavigate()
+    const handleOnClick=(e)=>{
+        e.preventDefault();
+        axios.post('http://localhost:2020/user/Logout',{},{withCredentials:true})
+        .then((res)=>{
+            if (res.data.success){
+
+                toast.success(res.data.message)
+                setTimeout(() => {
+                 navigate('/signin')
+                }, 1500);
+
+            }
+        })
+    }
+
     return(
         <div className="d-flex justify-content-center align-items-center flex-column vh-100">
+            <ToastContainer position="top-right" autoClose={3000} />
             <h3>Sorry, too lazy to make the whole settings :p</h3>
-            <button className="btn btn-danger">Logout</button>
+            <button className="btn btn-danger" onClick={(e)=>{handleOnClick(e)}}>Logout</button>
         </div>
     )
 }
